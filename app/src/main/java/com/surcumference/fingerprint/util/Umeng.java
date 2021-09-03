a80d5af2c16ff2d81d2ca2a52e41993deafd74d0
fei-ke <jinyang656@qq.com>
2021-09-03 11:49:54 +0800

    remove Umeng

--- app/src/main/java/com/surcumference/fingerprint/util/Umeng.java
+++ app/src/main/java/com/surcumference/fingerprint/util/Umeng.java
@@ -3,7 +3,7 @@ package com.surcumference.fingerprint.util;
 import android.content.Context;
 
 import com.surcumference.fingerprint.util.log.L;
-import com.umeng.analytics.MobclickAgent;
+//import com.umeng.analytics.MobclickAgent;
 
 /**
  * Created by Jason on 2017/9/11.
@@ -15,11 +15,11 @@ public class Umeng {
 
     public static void init(Context context) {
         sContext = context;
-        try {
-            MobclickAgent.startWithConfigure(new MobclickAgent.UMAnalyticsConfig(context.getApplicationContext(), "59b68c81f5ade45de90004f7", context.getPackageName()));
-        } catch (Exception e) {
-            L.e(e);
-        }
+        //try {
+        //    MobclickAgent.startWithConfigure(new MobclickAgent.UMAnalyticsConfig(context.getApplicationContext(), "59b68c81f5ade45de90004f7", context.getPackageName()));
+        //} catch (Exception e) {
+        //    L.e(e);
+        //}
     }
 
     public static void reportError(String message) {
@@ -28,14 +28,14 @@ public class Umeng {
             L.d("context not set");
             return;
         }
-        MobclickAgent.reportError(context, message);
+        //MobclickAgent.reportError(context, message);
     }
 
     public static void onResume(Context context) {
-        MobclickAgent.onResume(context);
+        //MobclickAgent.onResume(context);
     }
 
     public static void onPause(Context context) {
-        MobclickAgent.onPause(context);
+        //MobclickAgent.onPause(context);
     }
 }
